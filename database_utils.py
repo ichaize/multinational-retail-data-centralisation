b860fac2eb5a1f737c22120d97aea43e7df1a197
ichaize <isabellechaize1@gmail.com>
2023-05-31 13:06:23 +0100

    Creates init_db_engine method in DatabaseConnector that uses read_db_creds to return a sqlalchemy engine using the credentials from db_creds.yaml

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,5 @@
 import yaml
+from sqlalchemy import create_engine
 
 class DatabaseConnector:
 
@@ -7,6 +8,18 @@ class DatabaseConnector:
             db_creds = yaml.safe_load(stream)
         return db_creds
     
+    def init_db_engine(self):
+        creds = self.read_db_creds()
+        user = creds["RDS_USER"]
+        password = creds["RDS_PASSWORD"]
+        host = creds["RDS_HOST"]
+        port = creds["RDS_PORT"]
+        database = creds["RDS_DATABASE"]
+        engine = create_engine(url="postgresql://{0}:{1}@{2}:{3}/{4}".format(
+                                    user, password, host, port, database))
+        return engine
+    
 DC = DatabaseConnector()
-DC.read_db_creds()
+engine = DC.init_db_engine()
+engine.connect()
 
