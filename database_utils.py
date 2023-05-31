9c31343a2dfb2a5b02b9bfd4caf089048c9c3c9e
ichaize <isabellechaize1@gmail.com>
2023-05-31 12:36:59 +0100

    Creates read_db_creds method in DatabaseConnector to read db_creds.yaml

--- database_utils.py
+++ database_utils.py
@@ -1,1 +1,12 @@
+import yaml
+
 class DatabaseConnector:
+
+    def read_db_creds(self):
+        with open("db_creds.yaml") as stream:
+            db_creds = yaml.safe_load(stream)
+        return db_creds
+    
+DC = DatabaseConnector()
+DC.read_db_creds()
+
