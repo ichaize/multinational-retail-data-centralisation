20e02a737c6419f53d1e70e9b5dc82deba786df0
ichaize <isabellechaize1@gmail.com>
2023-06-07 17:25:38 +0100

    Adds method to retrieve number of stores  from an API

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,8 @@
 from database_utils import DatabaseConnector
 import pandas as pd
 import tabula
+import requests
+
 
 class DataExtractor:
 
@@ -13,12 +15,19 @@ class DataExtractor:
         dfs = tabula.read_pdf(link, pages="all")
         df = pd.concat(dfs)
         return df
+    
+    def list_number_of_stores(self, endpoint, header):
+        response = requests.get(endpoint, header)
+        return response.text
         
 
 DE = DataExtractor()
 DC = DatabaseConnector()
-# tables = DC.list_db_tables()
 user_data = DE.read_rds_table(DC, "legacy_users")
+
 pdf_link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
 card_data = DE.retrieve_pdf_data(pdf_link)
-print(card_data.shape[0])
+
+header_dict = {"x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
+number_of_stores = DE.list_number_of_stores("https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores", header=header_dict)
+print(number_of_stores)
